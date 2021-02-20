a4707880f7f8d8151bf69a5afc2dcab54c0e820d
lolitagupta <aliyanura9@gmail.com>
2021-02-20 17:35:45 +0600

    last commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
